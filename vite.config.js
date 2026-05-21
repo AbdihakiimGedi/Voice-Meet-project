479bc526223cc58bdacae2ae6d833fd0a39af547
Abdihakiim Gedi <apoousta88@gmail.com>
2026-05-21 20:13:15 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Voice-Meet-project/',
   plugins: [
     react(),
     tailwindcss(),
